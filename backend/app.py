c6a21e67944edd113f9697e8013b4c24d71925e4
dilmith-laktharana <dilmithlaktharana1@gmail.com>
2026-01-01 21:12:13 +0530

    Created Appointment Model For Database

--- backend/app.py
+++ backend/app.py
@@ -33,8 +33,20 @@ class Child(db.Model):
     date_of_birth = db.Column(db.Date, nullable=False)
     gender = db.Column(db.String(10), nullable=False)
 
+    
+    
 
 
+class Appointment(db.Model):
+    __tablename__ = 'appointment'
+    id = db.Column(db.Integer, primary_key=True)
+    child_id = db.Column(db.Integer, db.ForeignKey('child.id'), nullable=False)
+    appointment_type = db.Column(db.String(50), nullable=False) 
+    appointment_date = db.Column(db.DateTime, nullable=False)
+    doctor_name = db.Column(db.String(100))
+    status = db.Column(db.String(20), default='scheduled') 
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)    
 
 with app.app_context():
     db.create_all()
