e6cd8bd24b64c43232b16d31e14cb1eac0c683d1
Thinal <r.thinal.fernando@gmail.com>
2026-01-02 18:25:04 +0530

    updated DashboardHeader.jsx to get and display the data of the logged in child from the data sent by the backend

--- frontend/src/parent/Components/DashBoardHeader.jsx
+++ frontend/src/parent/Components/DashBoardHeader.jsx
@@ -2,16 +2,36 @@ import style from "../../assets/styleSheets/ParentDashboard.module.css";
 import React, { useEffect, useState } from "react";
 
 function DashboardHeader() {
+  const [childName, setChildName] = useState("");
+  const [childAge, setChildAge] = useState("");
+  const [childGender, setChildGender] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/header", { credentials: "include" })
+      .then((result) => result.json())
+      .then((data) => {
+        setChildName(data.name);
+        setChildAge(data.date_of_birth);
+        setChildGender(data.gender);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
-    <header className={style.dashboardHeader}>
+    <header
+      className={style.dashboardHeader}
+      style={{
+        backgroundColor: "#4A90E2",
+      }}
+    >
       <div className={style.headerContent}>
         <div className={style.childInfoSection}>
           <div className={style.childIcon}>
             {/* change to show the pic the parent adds later from the profile section and maybe add like a default icon to show if a image int added */}
           </div>
           <div className={style.childInfo}>
-            <h1 className={style.childName}>Thinal Fernando</h1>
-            <div className={style.childAge}>10 months 12 days</div>
+            <h1 className={style.childName}>{childName}</h1>
+            <div className={style.childAge}>{childAge}</div>
           </div>
         </div>
         <div className={style.headerActionsSection}>
