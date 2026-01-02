512157f6f68e59c1a430fc9c9e31398c8d527e9c
Thinal <r.thinal.fernando@gmail.com>
2026-01-02 17:28:50 +0530

    changed the css to follow modules to streamline development and create a index.css to hold common style, also lined the parentLayout to the home page

--- frontend/src/layouts/ParentLayout.jsx
+++ frontend/src/layouts/ParentLayout.jsx
@@ -1,3 +1,8 @@
-export default function ParentLayout() {
-  return <h2>Parent Dashboard</h2>;
+import Home from "../parent/Pages/home";
+import "../assets/styleSheets/ParentDashboard.module.css";
+
+function ParentLayout() {
+  return <Home />;
 }
+
+export default ParentLayout;
