30edd05ac407ea319d1239aff76857d4355d9ec7
Thinal <r.thinal.fernando@gmail.com>
2026-01-02 19:47:24 +0530

    - created a basic side bar - setup classes and import for styling

--- frontend/src/parent/Components/DashboardNav.jsx
+++ frontend/src/parent/Components/DashboardNav.jsx
@@ -1,12 +1,40 @@
-
-
+import style from "../../assets/styleSheets/ParentDashboard.module.css";
 
 function SideNav() {
+  const menuItems = [
+    { name: "Home", icon: "bi-house-door-fill" },
+    { name: "Analytics", icon: "bi-graph-up" },
+    { name: "Milestones", icon: "bi-trophy-fill" },
+    { name: "AI Analytics", icon: "bi-robot" },
+    { name: "Education", icon: "bi-book-half" },
+    {
+      name: "Message Doctor",
+      icon: "bi-chat-dots-fill",
+    },
+    { name: "More", icon: "bi-three-dots" },
+  ];
 
-    return(
-        <p></p>
-    )
+  return (
+    <nav className={style.mainMenu}>
+      <div className={style.area}>
+        <ul>
+          {menuItems.map((item) => (
+            <li key={item.name}>
+              <i className={`bi ${item.icon}`}></i>
+              <span className="nav-text">{item.name}</span>
+            </li>
+          ))}
+        </ul>
 
+        <ul className={style.logout}>
+          <li>
+            <i className="bi bi-box-arrow-left"></i>
+            <span className={style.navText}>Logout</span>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default SideNav
+export default SideNav;
