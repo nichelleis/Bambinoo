16ff1892b09e046c596789ec89b0ca8bcddbee24
Thinal <r.thinal.fernando@gmail.com>
2026-01-03 12:28:32 +0530

    Created a basic overview card using bootstrap and added basic styling for now

--- frontend/src/parent/Components/OverviewComponent.jsx
+++ frontend/src/parent/Components/OverviewComponent.jsx
@@ -1,9 +1,60 @@
-
-
 function Overview() {
+  return (
+    <div className="card">
+      <div className="d-flex justify-content-between align-items-center">
+        <div className="card-title">
+          <span className="card-icon">
+            <i className="bi bi-bar-chart-line-fill"></i>
+          </span>
+          Growth Overview
+        </div>
+        <button className="btn btn-sm text-primary">
+          View Details <i className="bi bi-arrow-right"></i>
+        </button>
+      </div>
 
+      <div className="row card-body text-center mb-1">
+        <div className="col-4">
+          <div
+            className=" p-4 rounded"
+            style={{ background: "rgba(107, 99, 255, 0.2)" }}
+          >
+            <div className="fs-2 fw-bold text-primary"> kg</div>
+            <div className="text-muted small">Weight</div>
+            <div className="text-success small">
+              <i className="fas fa-arrow-up"></i> + kg
+            </div>
+          </div>
+        </div>
 
+        <div className="col-4">
+          <div
+            className="stat-card p-4 rounded"
+            style={{ background: "rgba(255, 107, 156, 0.2)" }}
+          >
+            <div className="fs-2 fw-bold text-secondary">cm</div>
+            <div className="text-muted small">Height</div>
+            <div className="text-success small">
+              <i className="fas fa-arrow-up"></i> +cm
+            </div>
+          </div>
+        </div>
 
+        <div className="col-4">
+          <div
+            className=" p-4 rounded"
+            style={{ background: "rgba(254, 192, 99, 0.2)" }}
+          >
+            <div className="fs-2 fw-bold text-warning">cm</div>
+            <div className="text-muted small">Head Circumference</div>
+            <div className="text-success small">
+              <i className="fas fa-arrow-up"></i> + cm
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Overview
+export default Overview;
