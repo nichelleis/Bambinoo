ab5d3aa02671c12b3d69590dcaf3c03a32f78c16
Thinal <r.thinal.fernando@gmail.com>
2026-01-03 12:19:17 +0530

    Created Overview Component template and added it to a proper layout in the home page

--- frontend/src/parent/Pages/home.jsx
+++ frontend/src/parent/Pages/home.jsx
@@ -1,11 +1,16 @@
 import DashboardHeader from "../Components/DashBoardHeader";
 import DashboardNav from "../Components/DashboardNav";
+import Overview from "../Components/OverviewComponent";
 
 function Home() {
   return (
-    <>
-    
-    </>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-8 mb-4">
+          <Overview />
+        </div>
+      </div>
+    </div>
   );
 }
 
