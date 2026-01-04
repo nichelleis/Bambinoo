59b1854269417e4cf2510867f91f9ec395e4c509
Thinal <r.thinal.fernando@gmail.com>
2026-01-04 12:40:26 +0530

    added a super basic logout button to each dashboard for easier development

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -1,3 +1,15 @@
 export default function AdminLayout() {
-  return <h2>Admin Dashboard</h2>;
+  return (
+    <div>
+      <h2>Admin Dashboard</h2>
+      <button
+        onClick={() => {
+          localStorage.removeItem("token");
+          window.location.href = "/";
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
 }
