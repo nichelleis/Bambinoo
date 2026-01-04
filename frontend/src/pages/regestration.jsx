0e4c5fec2f49e6ea43fb60aa9e9b99b44e353ce6
Lakindu Jayathilaka <lakindu.20241937@iit.ac.lk>
2026-01-05 01:30:57 +0530

    update registration model and adjust API endpoint in frontend

--- frontend/src/pages/regestration.jsx
+++ frontend/src/pages/regestration.jsx
@@ -1,4 +1,3 @@
-// Registration.jsx
 import React, { useState } from 'react';
 import style from '../assets/styleSheets/Registration.module.css';
 
@@ -126,7 +125,7 @@ const Registration = () => {
     };
 
     try {
-      const response = await fetch("http://localhost:5000/api/pending-registration", { // change the fetch link to the databse
+      const response = await fetch("http://127.0.0.1:5000/api/pending_registration", { // change the fetch link to the databse
         method: "POST",
         headers: {
           "Content-Type": "application/json"
