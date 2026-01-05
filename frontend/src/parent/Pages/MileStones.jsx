49b627b6b0a834d3195098ca998a933a32ac467f
Thinal <r.thinal.fernando@gmail.com>
2026-01-05 12:53:22 +0530

    created the filter age section with buttons to select the agegroup

--- frontend/src/parent/Pages/MileStones.jsx
+++ frontend/src/parent/Pages/MileStones.jsx
@@ -16,6 +16,32 @@ function Milestones() {
           </div>
         </div>
       </div>
+
+      <div className={`${style.ageGroupFilter} ${style.dashboardCard}`}>
+        <div className={style.filterLabel}>
+          <i className="bi bi-funnel me-2"></i>
+          Filter by Age
+        </div>
+        <div className={style.ageGroupButtons}>
+          <button className={style.ageBtn}>All Ags</button>
+          <button className={style.ageBtn}>Birth</button>
+          <button className={style.ageBtn}>1 month</button>
+          <button className={style.ageBtn}>2 months</button>
+          <button className={style.ageBtn}>4 months</button>
+          <button className={style.ageBtn}>6 months</button>
+          <button className={style.ageBtn}>7 months</button>
+          <button className={style.ageBtn}>9 months</button>
+          <button className={style.ageBtn}>10 months</button>
+          <button className={style.ageBtn}>1 year</button>
+          <button className={style.ageBtn}>15 months</button>
+          <button> className={style.ageBtn}18 months</button>
+          <button className={style.ageBtn}>2years</button>
+          <button className={style.ageBtn}>30 months</button>
+          <button className={style.ageBtn}>3years</button>
+          <button className={style.ageBtn}>4 years</button>
+          <button className={style.ageBtn}>5 years</button>
+        </div>
+      </div>
     </div>
   );
 }
