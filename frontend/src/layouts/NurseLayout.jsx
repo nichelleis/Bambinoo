97f9232b6df773a87c271fe90c9d7fd46ea6fa97
yasadya-lakdini <r.p.yasadya2005@gmail.com>
2026-03-01 20:48:04 +0530

    Eroor handeling Nurse

--- frontend/src/layouts/NurseLayout.jsx
+++ frontend/src/layouts/NurseLayout.jsx
@@ -1,1 +1,15 @@
-c
+export default function NurseLayout() {
+  return (
+    <div>
+      <h2>Nurse Dashboard</h2>
+      <button
+        onClick={() => {
+          localStorage.removeItem("token");
+          window.location.href = "/";
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
+}
